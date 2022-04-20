35899bcfe266564f0f0835e69432d333a2e74184
Patrick Kunzmann <patrick.kunzm@gmail.com>
2022-04-20 16:45:32 +0200

    Add Hessian matrix calculation

--- src/springcraft/interaction.py
+++ src/springcraft/interaction.py
@@ -12,6 +12,44 @@ import biotite.structure as struc
 
 
 def compute_kirchhoff(coord, force_field, cutoff_distance, use_cell_list=True):
+    pairs, _, sq_dist = _prepare_values_for_interaction_matrix(
+        coord, force_field, cutoff_distance, use_cell_list
+    )
+
+    kirchhoff = np.zeros((len(coord), len(coord)))
+    force_constants = force_field.force_constant(
+        pairs[:,0], pairs[:,1], sq_dist
+    )
+    kirchhoff[pairs[:,0], pairs[:,1]] = -force_constants
+    # Set values for main diagonal
+    np.fill_diagonal(kirchhoff, -np.sum(kirchhoff, axis=0))
+    
+    return kirchhoff, pairs
+
+
+def compute_hessian(coord, force_field, cutoff_distance, use_cell_list=True):
+    pairs, disp, sq_dist = _prepare_values_for_interaction_matrix(
+        coord, force_field, cutoff_distance, use_cell_list
+    )
+
+    # Hessian matrix has 3x3 matrices as superelements
+    hessian = np.zeros((len(coord), len(coord), 3, 3))
+    force_constants = force_field.force_constant(
+        pairs[:,0], pairs[:,1], sq_dist
+    )
+    hessian[pairs[:,0], pairs[:,1]] = (
+        -force_constants[:, np.newaxis, np.newaxis]
+        / sq_dist[:, np.newaxis, np.newaxis]
+        * disp[:, :, np.newaxis] * disp[:, np.newaxis, :]
+    )
+    # Set values for main diagonal
+    np.fill_diagonal(hessian, -np.sum(hessian, axis=0))
+    
+    return hessian, pairs
+
+
+def _prepare_values_for_interaction_matrix(coord, force_field, cutoff_distance,
+                                           use_cell_list=True):
     if coord.ndim != 2 or coord.shape[1] != 3:
         raise ValueError(
             f"Expected coordinates with shape (n,3), got {coord.shape}"
@@ -29,31 +67,24 @@ def compute_kirchhoff(coord, force_field, cutoff_distance, use_cell_list=True):
         atom_i, atom_j = np.where(adj_matrix)
     else:
         # Brute force: Calculate all pairwise squared distances
-        disp = struc.displacement(
+        disp_matrix = struc.displacement(
             coord[np.newaxis, :, :], coord[:, np.newaxis, :]
         )
-        sq_dist_matrix = np.sum(disp*disp, axis=-1)
+        sq_dist_matrix = np.sum(disp_matrix * disp_matrix, axis=-1)
         atom_i, atom_j = np.where(sq_dist_matrix <= cutoff_distance**2)
     pairs = np.array((atom_i, atom_j)).T
     
     # Remove interactions of atoms with itself
     pairs = pairs[pairs[:, 0] != pairs[:, 1]]
 
-    # Get squared distances for distance-dependent force fields
+    # Get displacement vector for ANMs
+    # and squared distances for distance-dependent force fields
     if use_cell_list:
         disp = struc.index_displacement(coord, pairs)
         sq_dist = np.sum(disp*disp, axis=-1)
     else:
-        # Squared distances were already calculated
+        # Displacements and squared distances were already calculated
+        disp = disp_matrix[pairs[:,0], pairs[:,1]]
         sq_dist = sq_dist_matrix[pairs[:,0], pairs[:,1]]
-    
-    # Fill Kirchhoff matrix
-    kirchhoff = np.zeros((len(coord), len(coord)))
-    force_constants = force_field.force_constant(
-        pairs[:,0], pairs[:,1], sq_dist
-    )
-    kirchhoff[pairs[:,0], pairs[:,1]] = -force_constants
-    # Set values for main diagonal
-    np.fill_diagonal(kirchhoff, -np.sum(kirchhoff, axis=0), wrap=False)
 
-    return kirchhoff, pairs
+    return pairs, disp, sq_dist
