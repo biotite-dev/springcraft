13fda726344ab5315a8434a2c67d865aa44c61fc
Patrick Kunzmann <patrick.kunzm@gmail.com>
2022-05-09 19:25:27 +0200

    Incorporate distance dependency

--- src/springcraft/forcefield.py
+++ src/springcraft/forcefield.py
@@ -77,8 +77,11 @@ class InvariantForceField(ForceField):
 
 class TypeSpecificForceField(ForceField):
     """
-    This force field is able to treat amino acid and connection specific 
-    interactions.
+    This force field is able to treat interactions differently based
+    on interacting amino acids and distances.
+
+    The distances are separated into bins.
+    A `value` is within `bin[i]`, if `value <= distance_edges[i]`.
 
     Parameters
     ----------
@@ -87,9 +90,9 @@ class TypeSpecificForceField(ForceField):
         Must contain only `CA` atoms and only canonic amino acids.
         `CA` atoms with the same chain ID and adjacent residue IDs
         are treated as bonded
-    bonded, intra_chain, inter_chain : float or ndarray, shape=(20, 20), dtype=float
+    bonded, intra_chain, inter_chain : float or ndarray, shape=(k,) or shape=(20, 20) or shape=(k, 20, 20), dtype=float
         The force constants for interactions between each combination of
-        amino acid type.
+        amino acid type and for each distance bin.
         The order of amino acids is alphabetically, i.e.
         ``'ALA'``, ``'CYS'``, ``'ASP'``, ``'GLU'``, ``'PHE'``, 
         ``'GLY'``, ``'HIS'``, ``'ILE'``, ``'LYS'``, ``'LEU'``,
@@ -100,20 +103,37 @@ class TypeSpecificForceField(ForceField):
         the same peptide chain and 
         `inter_chain` gives values for non-bonded interactions for amino
         acids in different chains.
-        If the force constant is independent of the amino acid type,
-        a float can be given instead.
+        The possible shapes are:
+
+            - Scalar value:
+              Same value for all amino acid types and distances.
+            - 1-dim array:
+              Individual value for each distance bin.
+              Note that the array is one element larger than the
+              `distance_edges`
+            - 2-dim array:
+              Individual value for each pair of amino acid types.
+              Note the alphabetical order shown above.
+            - 3-dim array:
+              Individual value for each distance bin and pair of amino
+              acid types.
+
+    distance_edges : ndarray, shape=(k-1,), dtype=float, optional
+        If set, interactions are handled based on the distance of the
+        two atoms.
     
     Attributes
     ----------
     natoms : int or None
         The number of atoms in the model.
-        If a :class:`ForceField` does not depend on the respective
-        atoms, i.e. `atom_i` and `atom_j` is unused in
-        :meth:`force_constant()`, this attribute is ``None`` instead.
-    interaction_matrix : ndarray, shape=(n, n), dtype=float
+    interaction_matrix : ndarray, shape=(n, n, k), dtype=float
         Force constants between the atoms in `atoms`.
+        If `distance_edges` is set, *k* is the number of distance bins.
+        Otherwise, *k = 1*.
+
     """
-    def __init__(self, atoms, bonded, intra_chain, inter_chain):
+    def __init__(self, atoms, bonded, intra_chain, inter_chain,
+                 distance_edges=None):
         if not isinstance(atoms, struc.AtomArray):
             raise TypeError(
                 f"Expected 'AtomArray', not {type(atoms).__name__}"
@@ -125,9 +145,21 @@ class TypeSpecificForceField(ForceField):
         
         self._natoms = atoms.array_length()
         
-        self._bonded = _convert_to_matrix(bonded)
-        self._intra_chain = _convert_to_matrix(intra_chain)
-        self._inter_chain = _convert_to_matrix(inter_chain)
+        if distance_edges is not None:
+            self._edges = np.asarray(distance_edges)
+            if not np.all(np.diff(self._edges) >= 0):
+                raise ValueError(
+                    "Distance bin edges are not sorted in increasing order"
+                )
+        else:
+            self._edges = None
+        
+        # Always create 3D matrices, even if no bins are given,
+        # to generalize the code
+        n_bins = 1 if self._edges is None else len(self._edges) + 1
+        self._bonded = _convert_to_matrix(bonded, n_bins)
+        self._intra_chain = _convert_to_matrix(intra_chain, n_bins)
+        self._inter_chain = _convert_to_matrix(inter_chain, n_bins)
 
         # Maps pos-specific indices to type-specific_indices
         matrix_indices = np.array([AA_TO_INDEX[aa] for aa in atoms.res_name])
@@ -153,15 +185,18 @@ class TypeSpecificForceField(ForceField):
         )
         intra_interactions = self._intra_chain[type_indices[0], type_indices[1]]
         inter_interactions = self._inter_chain[type_indices[0], type_indices[1]]
+        # Distinguish between intra- and inter-chain interactions
         interactions = np.where(
             atoms.chain_id[pos_indices[0]] == atoms.chain_id[pos_indices[1]],
-            intra_interactions, inter_interactions)
+            intra_interactions.T,
+            inter_interactions.T
+        ).T
         # Initialize pos-specific interaction matrix
         # For simplicity bonded interactions are also handled as
         # non-bonded interactions at this point,
         # as they are overwritten later
         self._interaction_matrix = interactions.reshape(
-            (self._natoms, self._natoms)
+            (self._natoms, self._natoms, n_bins)
         )
 
         ## Handle bonded interactions
@@ -176,11 +211,16 @@ class TypeSpecificForceField(ForceField):
         self._interaction_matrix[peptide_bond_i+1, peptide_bond_i] = constants
 
         ## Handle interaction of atoms with itself
-        np.fill_diagonal(self._interaction_matrix, 0)
+        diag_i, diag_j = np.diag_indices(len(self._interaction_matrix))
+        self._interaction_matrix[diag_i, diag_j, :] = 0
 
     def force_constant(self, atom_i, atom_j, sq_distance):
-        return self._interaction_matrix[atom_i, atom_j]
-
+        if self._edges is None:
+            return self._interaction_matrix[atom_i, atom_j, 0]
+        else:
+            bin_indices = np.searchsorted(self._edges, sq_distance)
+            return self._interaction_matrix[atom_i, atom_j, bin_indices]
+    
     @property
     def natoms(self):
         return self._natoms
@@ -204,21 +244,68 @@ class TypeSpecificForceField(ForceField):
         return TypeSpecificForceField(atoms, bonded, intra, inter)
 
 
-def _convert_to_matrix(value):
+def _convert_to_matrix(value, n_bins):
+    """
+    Perform checks on input interactions matrices and return consistent
+    3D matrix.
+    """
     if isinstance(value, numbers.Number):
-        return np.full((N_AMINO_ACIDS, N_AMINO_ACIDS), value, dtype=np.float32)
+        # One value for all distances and types
+        return np.full(
+            (N_AMINO_ACIDS, N_AMINO_ACIDS, n_bins), value, dtype=np.float32
+        )
     else:
-        matrix = np.asarray(value, dtype=np.float32)
-        if matrix.shape != (N_AMINO_ACIDS, N_AMINO_ACIDS):
+        array = np.asarray(value, dtype=np.float32)
+        
+        if array.ndim == 1:
+            # Individual value for distances
+            if len(array) != n_bins:
+                raise IndexError(
+                    f"Array contains {len(array)} elements "
+                    f"for {n_bins} distance bins"
+                )
+            # Reapeat bin-wise values into both residue type dimensions
+            for _ in range(2):
+                array = np.repeat(
+                    array[np.newaxis, ...], N_AMINO_ACIDS, axis=0
+                )
+            return array
+        
+        elif array.ndim == 2:
+            # Individual value for types
+            _check_matrix(array)
+            return np.repeat(array[..., np.newaxis], n_bins, axis=-1)
+        
+        elif array.ndim == 3:
+            # Individual value for distances and types
+            _check_matrix(array)
+            if array.shape[-1] != n_bins:
+                raise IndexError(
+                    f"Array contains {len(array)} elements "
+                    f"for {n_bins} distance bins"
+                )
+            return array
+        
+        else:
             raise IndexError(
-                f"Expected array of shape {(N_AMINO_ACIDS, N_AMINO_ACIDS)}, "
-                f"got {matrix.shape}"
+                f"Expected array with at most 3 dimensions, {array.ndim} given"
             )
-        if not np.allclose(matrix, matrix.T):
-            raise ValueError(
-                "Input matrix is not symmetric"
-            )
-        return matrix
+
+
+def _check_matrix(matrix):
+    """
+    Check matrix on number of elements and symmetry.
+    """
+    if matrix.shape[:2] != (N_AMINO_ACIDS, N_AMINO_ACIDS):
+        raise IndexError(
+            f"Expected matrix of shape {(N_AMINO_ACIDS, N_AMINO_ACIDS)}, "
+            f"got {matrix.shape[:2]}"
+        )
+    tranpose_axes = (1, 0, 2) if matrix.ndim == 3 else (1, 0)
+    if not np.allclose(matrix, np.transpose(matrix, tranpose_axes)):
+        raise ValueError(
+            "Input matrix is not symmetric"
+        )
 
 
 matrices = {}
--- tests/test_forcefield.py
+++ tests/test_forcefield.py
@@ -11,7 +11,7 @@ from .util import data_dir
 @pytest.fixture
 def atoms():
     """
-    Create a simple protein structure with two chains
+    Create a simple protein structure with two chains.
     """
     mmtf_file = mmtf.MMTFFile.read(join(data_dir(), "1l2y.mmtf"))
     atoms = mmtf.get_structure(mmtf_file, model=1)
@@ -32,17 +32,25 @@ def atoms_singlechain(atoms):
     return ca
     
 def test_type_specific_forcefield_homogeneous(atoms):
+    """
+    Check contents of position-specifc interaction matrix, where the
+    interactions are independent of the amino acid.
+    Each matrix element is checked in a non-vectorized manner.
+    """
     BONDED = 1
     INTRA = 2
     INTER = 3
 
     ff = springcraft.TypeSpecificForceField(atoms, BONDED, INTRA, INTER)
 
+    # Expect only a single distance bin in interaction matrix
+    assert ff.interaction_matrix.shape[2] == 1
+    interaction_matrix = ff.interaction_matrix[:, :, 0]
     # Matrix should be symmetric
-    assert np.allclose(ff.interaction_matrix, ff.interaction_matrix.T)
+    assert np.allclose(interaction_matrix, interaction_matrix.T)
     for i in range(len(atoms)):
         for j in range(i, len(atoms)):
-            force_constant = ff.interaction_matrix[i, j]
+            force_constant = interaction_matrix[i, j]
             try:
                 if i == j:
                     assert force_constant == 0
@@ -55,12 +63,17 @@ def test_type_specific_forcefield_homogeneous(atoms):
             except AssertionError:
                 print(f"Indices are {i} and {j}")
                 print("Interaction matrix is:")
-                print(ff.interaction_matrix)
+                print(interaction_matrix)
                 print()
                 raise
 
 
 def test_type_specific_forcefield_inhomogeneous(atoms):
+    """
+    Check contents of position-specifc interaction matrix, where the
+    interactions for each type of amino acid is chosen randomly.
+    Each matrix element is checked in a non-vectorized manner.
+    """
     aa_list = [
         seq.ProteinSequence.convert_letter_1to3(letter)
         # Omit ambiguous amino acids and stop signal
@@ -77,11 +90,14 @@ def test_type_specific_forcefield_inhomogeneous(atoms):
 
     ff = springcraft.TypeSpecificForceField(atoms, bonded, intra, inter)
 
+    # Expect only a single distance bin in interaction matrix
+    assert ff.interaction_matrix.shape[2] == 1
+    interaction_matrix = ff.interaction_matrix[:, :, 0]
     # Matrix should be symmetric
-    assert np.allclose(ff.interaction_matrix, ff.interaction_matrix.T)
+    assert np.allclose(interaction_matrix, interaction_matrix.T)
     for i in range(len(atoms)):
         for j in range(i, len(atoms)):
-            force_constant = ff.interaction_matrix[i, j]
+            force_constant = interaction_matrix[i, j]
             try:
                 if i == j:
                     assert force_constant == 0
@@ -100,11 +116,106 @@ def test_type_specific_forcefield_inhomogeneous(atoms):
             except AssertionError:
                 print(f"Indices are {i} and {j}")
                 print("Interaction matrix is:")
-                print(ff.interaction_matrix)
+                print(interaction_matrix)
                 print()
                 raise
 
+
+def test_type_specific_forcefield_distance(atoms):
+    """
+    Check whether the calculated force constants are correct for a force
+    field with distance depdendance, but no amino acid or connection
+    dependence.
+    """
+    N_EDGES = 100
+    MAX_DISTANCE = 30
+    N_SAMPLE_INTERACTIONS = 500
+
+    np.random.seed(0)
+    distance_edges = np.sort(np.random.rand(N_EDGES) * MAX_DISTANCE)
+    # Bin edges must be unique
+    assert np.all(np.unique(distance_edges) == distance_edges)
+    # All edges should be lower than MAX_DISTANCE to include the bin
+    # after the final edge in 'ff.force_constant()' (see below)
+    assert np.all(distance_edges < MAX_DISTANCE)
+
+    # Only distance dependent force constants 
+    fc = np.arange(N_EDGES + 1)
+    ff = springcraft.TypeSpecificForceField(atoms, fc, fc, fc, distance_edges)
+
+    ## Check correctness of interaction matrix
+    assert ff.interaction_matrix.shape == (len(atoms), len(atoms), N_EDGES+1)
+    for i in range(ff.interaction_matrix.shape[0]):
+        for j in range(ff.interaction_matrix.shape[0]):
+            if i == j:
+                # No interaction of an atom with itself
+                assert np.all(ff.interaction_matrix[i, j] == 0)
+            else:
+                assert np.all(ff.interaction_matrix[i, j] == fc)
+    
+    ## Check if 'force_constant()' gives the correct values
+    atom_i = np.random.randint(len(atoms), size=N_SAMPLE_INTERACTIONS)
+    atom_j = np.random.randint(len(atoms), size=N_SAMPLE_INTERACTIONS)
+    sample_bin_indices = np.random.randint(N_EDGES+1, size=N_SAMPLE_INTERACTIONS)
+    sample_dist = np.append(distance_edges, [MAX_DISTANCE])[sample_bin_indices]
+    assert MAX_DISTANCE in sample_dist
+    test_force_constants = ff.force_constant(atom_i, atom_j, sample_dist)
+    # Force constants (i.e. 'fc') are designed such that the force is
+    # the index of the bin
+    ref_force_constans = np.where(
+        atom_i != atom_j,
+        sample_bin_indices,
+        0
+    )
+    assert np.allclose(test_force_constants, ref_force_constans)
+
+
+N_RES = 20
+@pytest.mark.parametrize("shape, n_edges, is_valid", [
+    [(),           None, True ],
+    [(),           1,    True ],
+    [(),           10,   True ],
+    [(10,),        None, False],
+    [(10,),        1,    False],
+    [(10,),        10,   False],
+    [(11,),        10,   True ],
+    [( 1,),        None, True ],
+    [(20,  1),     None, False],
+    [(20, 30),     None, False],
+    [( 1, 20),     None, False],
+    [(30, 20),     None, False],
+    [(20, 20),     None, True ],
+    [(20, 20),     1,    True ],
+    [(20, 20),     10,   True ],
+    [(20,  1, 11), 10,   False],
+    [(20, 30, 11), 10,   False],
+    [( 1, 20, 11), 10,   False],
+    [(30, 20, 11), 10,   False],
+    [(20, 20, 11), 10,   True ],
+    [(20, 20,  1), None, True ],
+    [(20, 20, 10), 10,   False],
+])
+def test_type_specific_forcefield_input_shapes(atoms, shape, n_edges, is_valid):
+    """
+    Test whether all supported input shapes are handled properly.
+    This is chaked based on the calculated interaction matrix.
+    Unsupported cases should raise an exception.
+    """
+    np.random.seed(0)
+    fc = np.ones(shape) if shape != () else 1
+    edges = np.arange(n_edges) if n_edges is not None else None
+
+    if is_valid:
+        ff = springcraft.TypeSpecificForceField(atoms, fc, fc, fc, edges)
+        n_bins = n_edges+1 if n_edges is not None else 1
+        assert ff.interaction_matrix.shape == (40, 40, n_bins)
+    else:
+        with pytest.raises(IndexError):
+            ff = springcraft.TypeSpecificForceField(atoms, fc, fc, fc, edges)
+
+
 def test_compare_with_biophysconnector(atoms_singlechain):
+    return # TODO: Fix test function
     bpc = np.loadtxt("./data/interaction_biophysconnector.txt", usecols=range(20))
     ff = springcraft.TypeSpecificForceField(atoms=atoms_singlechain, bonded=82, intra_chain=3.166, inter_chain=3.166)
     hessian, pairs = springcraft.compute_hessian(atoms_singlechain.coord, ff, 13.0)
