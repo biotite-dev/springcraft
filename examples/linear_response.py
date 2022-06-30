e63a4f18ee08b5927b188288f08098cfc4d1af59
Patrick Kunzmann <patrick.kunzm@gmail.com>
2022-06-30 15:08:34 +0200

    Use builtin function from Ammolite

--- examples/linear_response.py
+++ examples/linear_response.py
@@ -8,25 +8,6 @@ import springcraft
 PDB_ID = "1MUG"
 
 
-from pymol import cmd, cgo, CmdException
-def draw_arrow(name, start, end, radius=0.1, head_radius=0.20, head_length=0.5,
-               color=(0.5, 0.5, 0.5)):
-        CYLINDER = 9.0
-        CONE = 27.0
-
-        normal = (end - start) / np.linalg.norm(end - start)
-        middle = end - normal * head_length
-
-        ammolite.cmd.load_cgo(
-            [
-                CYLINDER, *start, *middle, radius, *color, *color,
-                CONE, *middle, *end, head_radius, 0.0, *color, *color,
-                1.0, 0.0
-            ],
-            name
-        )
-
-
 # Load structure
 mmtf_file = mmtf.MMTFFile.read(rcsb.fetch(PDB_ID, "mmtf"))
 atoms = mmtf.get_structure(mmtf_file, model=1)
@@ -44,7 +25,6 @@ ammolite.launch_interactive_pymol()
 pymol_object = ammolite.PyMOLObject.from_structure(ca)
 
 start_coord = ca.coord
-end_coord = ca.coord + displacement
-for i in range(len(displacement)):
-    draw_arrow(f"vector{i}", start_coord[i], end_coord[i])
+end_coord = ca.coord + displacement * 10
+ammolite.draw_arrows(start_coord, end_coord)
 pymol_object.orient()
