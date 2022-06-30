Fix numerical issue in pseudoinverse

src/springcraft/anm.py
@@ -78,7 +78,9 @@ class ANM:
     @property
     def covariance(self):
         if self._covariance is None:
-            self._covariance = np.linalg.pinv(self.hessian)
+            self._covariance = np.linalg.pinv(
+                self.hessian, hermitian=True, rcond=1e-6
+            )
         return self._covariance
     
     @covariance.setter

src/springcraft/gnm.py
@@ -76,7 +76,9 @@ class GNM:
     @property
     def covariance(self):
         if self._covariance is None:
-            self._covariance = np.linalg.pinv(self.kirchhoff)
+            self._covariance = np.linalg.pinv(
+                self.kirchhoff, hermitian=True, rcond=1e-6
+            )
         return self._covariance
     
     @covariance.setter