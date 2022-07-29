80d45670dc43fc6f544a2f6d8b4c94ef49bd0554
--local <jhkr@web.de>
2022-07-29 15:18:22 +0200

    Omit correct number of eigenvals

--- tests/test_forcefield.py
+++ tests/test_forcefield.py
@@ -446,5 +446,4 @@ def test_compare_eigenvals(atoms_singlechain):
     )
 
     # Omit modes belonging to trivial modes in reference -> Numerical deviations
-    # (automatically omitted in ANM.eigen())
-    assert np.allclose(test_eigenval, ref_eigenval[6:])
+    assert np.allclose(test_eigenval[6:], ref_eigenval[6:])
